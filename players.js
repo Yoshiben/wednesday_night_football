1d18e0d240f9598fce90922f5a525add5ed11f88
Yoshiben <48318488+Yoshiben@users.noreply.github.com>
2025-04-18 16:33:23 +0100

    added files - data, history, index, players

---
+++ players.js
@@ -0,0 +1,5 @@
+const defaultPlayers = [
+  "Bailey Fulton", "Ben Attle", "Bradley Travis", "Chad Ogden", "Craig Ingham", "Danny Minshull", "Dom Johnson", "James Shooter", 
+  "Joe Duffy", "Joe Kinnear", "Joe Rawlins", "John Sheard (Shed)", "Jorge Westin-Pearson", "Kyle Carrington", "Leon Khan", 
+  "Niall Raymond", "Ross Barney", "Sohrob Rostampour", "Tom Kelton", "Tom Wright", "Zach Murphy"
+];
